487c2f640642dcdef9fb3c07d7ed5f68bbe8c1cd
NickZRS <nick1804js@gmail.com>
2023-04-05 23:10:59 -0300

    imc_03

--- src/app/imc/imc.component.ts
+++ src/app/imc/imc.component.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './imc.component.html',
   styleUrls: ['./imc.component.css']
 })
+
 export class ImcComponent {
           peso : number = 0;
           alt : number = 0;
@@ -15,23 +16,23 @@ export class ImcComponent {
             this.resp = this.peso / (this.alt*this.alt)
 
             if(this.resp < 18.5){
-              document.getElementById("resp").value = this.resp.toFixed(2);
-              document.getElementById("imc").value = "Magreza";
-          }else if(imc >= 18.5 &&  imc <= 24.9){
-              document.getElementById("imc").value = imc.toFixed(2);
-              document.getElementById("resp").value = "Normal";
-          }else if(imc >= 25 &&  imc <= 29.9){
-              document.getElementById("imc").value = imc.toFixed(2);
-              document.getElementById("resp").value = "Sobrepeso";
-          }else if(imc >= 30 &&  imc <= 34.9){
-              document.getElementById("imc").value = imc.toFixed(2);
-              document.getElementById("resp").value = "Obesidade Grau I";
-          }else if(imc >= 35 &&  imc <= 39.9){
-              document.getElementById("imc").value = imc.toFixed(2);
-              document.getElementById("resp").value = "Obesidade Grau II";
+              (<HTMLInputElement>document.getElementById("resp")).value = this.resp.toFixed(2);
+              (<HTMLInputElement>document.getElementById("imc")).value = "Magreza";
+          }else if(this.resp >= 18.5 &&  this.resp <= 24.9){
+              (<HTMLInputElement>document.getElementById("resp")).value = this.resp.toFixed(2);
+              (<HTMLInputElement>document.getElementById("imc")).value = "Normal";
+          }else if(this.resp >= 25 &&  this.resp <= 29.9){
+              (<HTMLInputElement>document.getElementById("resp")).value = this.resp.toFixed(2);
+              (<HTMLInputElement>document.getElementById("imc")).value = "Sobrepeso";
+          }else if(this.resp >= 30 &&  this.resp <= 34.9){
+              (<HTMLInputElement>document.getElementById("resp")).value = this.resp.toFixed(2);
+              (<HTMLInputElement>document.getElementById("imc")).value = "Obesidade Grau I";
+          }else if(this.resp >= 35 && this.resp <= 39.9){
+              (<HTMLInputElement>document.getElementById("resp")).value = this.resp.toFixed(2);
+              (<HTMLInputElement>document.getElementById("imc")).value = "Obesidade Grau II";
           }else{
-              document.getElementById("imc").value = imc.toFixed(2);
-              document.getElementById("resp").value = "Obesidade Grau III";
+              (<HTMLInputElement>document.getElementById("resp")).value = this.resp.toFixed(2);
+              (<HTMLInputElement>document.getElementById("imc")).value = "Obesidade Grau III";
           }
           }
 
